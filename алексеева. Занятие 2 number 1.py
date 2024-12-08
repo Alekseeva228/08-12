b6c712efce1ddaf82d64b9b7c3242aca26f9e84f
Alekseeva228 <tumonya79@gmail.com>
2024-12-08 17:15:23 +0700

    Add files via upload

---
+++ алексеева. Занятие 2 number 1.py
@@ -0,0 +1,4 @@
+import time
+input("как тебя зовут?")
+time.sleep(2)
+print("А меня Ботяня!")
---
+++ алексеева. Занятие 2 number 3.py
@@ -0,0 +1,12 @@
+import time
+print("5")
+time.sleep(2)
+print("4")
+time.sleep(2)
+print("3")
+time.sleep(2)
+print("2")
+time.sleep(2)
+print("1")
+time.sleep(2)
+print("Да ладно, ничего страшного")
